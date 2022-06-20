7418d587b5c8c78e9101f2eaa6988fea7576481a
Adler <discord.1843@gmail.com>
2022-06-20 23:20:25 +0430

    client.py cleaned

--- client.py
+++ client.py
@@ -5,17 +5,16 @@ import random
 from tkinter import *
 
 # TODO esm haro ye taghir asasi bede
-# TODO error message haro dorost neshun bede
 # TODO be class hat vorudi bede
 # ------------------------------Functions-------------------------------
 
 # <-------Check True Validation------->
-def check_valid(win, IP, PORT, NAME):
+def check_valid(win,SERVER, IP, PORT, NAME):
     ip_pattern = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", IP)
     name_pattern = re.match(r"[A-Za-z1-9]", NAME)
 
     if ip_pattern and PORT.isdigit() and name_pattern:
-        return ser.connect_to_server(win, IP, int(PORT), NAME)
+        return SERVER.connect_to_server(win, IP, int(PORT), NAME)
     else:
         # TODO fix error message
         print('error')
@@ -31,10 +30,12 @@ def random_color():
 
 # ------------------------------Classes-------------------------------
 
-# <-------Server------->
-class server:
-    def __init__(self, soc):
+# <-------Server Class------->
+# TODO change this name ??
+class Server:
+    def __init__(self, soc:socket.socket, window):
         self.soc = soc
+        self.window=window
         self.RoomID = ''
         self.UserName = ''
         self.Color = random_color()
@@ -46,33 +47,32 @@ class server:
             data = f'connect {NAME}'.encode('utf-8')
             print(data)
             self.soc.sendall(data)
-
             response = str(self.soc.recv(1024), 'utf-8').split(' ')
             self.UserName = response[0]
-            window.insert_listbox(int(response[1]))
-
-            window.rooms_frame()
+            self.window.insert_listbox(int(response[1]))
+            self.window.rooms_frame()
             win.destroy()
             threading.Thread(target=self.client_recv).start()
         except:
             print('Something is wrong!')
             win.destroy()
 
-    def client_send(self, msg):
+    def client_send_message(self, msg):
         data = f'message {msg} {self.UserName} {self.Color} {self.RoomID}'
-        # data = self.pack_data('message',msg,name,color)
         try:
             self.soc.sendall(data.encode('utf-8'))
         except socket.error:
+            # TODO error message haro dorost neshun bede
             print('Server is down!')
     
     def client_create_room(self):
         data = f'create {self.UserName}'
         try:
             self.soc.sendall(data.encode('utf-8'))
-            window.room_frame.destroy()
-            window.chats_frame()
+            self.window.room_frame.destroy()
+            self.window.chats_frame()
         except socket.error:
+            # TODO error message haro dorost neshun bede
             print('Server is down!')
 
     def client_recv(self):
@@ -88,70 +88,71 @@ class server:
                 server_roomID = data[4]
                 if self.RoomID != server_roomID :
                     continue
-                # Custom tag color
-                window.MESSAGEBOX.tag_config(f'{server_username}', foreground=server_color)
 
-                # Insert message
-                window.MESSAGEBOX.configure(state='normal')
+                # Custom tag color for print with diffrent color
+                self.window.MESSAGEBOX.tag_config(f'{server_username}', foreground=server_color)
 
-                #? if username == window.USERNAME.get():
+                # Insert message in messagebox 
+                self.window.MESSAGEBOX.configure(state='normal')
                 if server_username == self.UserName:
-                    window.MESSAGEBOX.insert(END, f'Me: {server_message}\n')
+                    self.window.MESSAGEBOX.insert(END, f'Me: {server_message}\n')
                 else:
-                    window.MESSAGEBOX.insert(INSERT, f'{server_username}: {server_message}\n', f'{server_username}')
-                window.MESSAGEBOX.configure(state='disabled')
+                    self.window.MESSAGEBOX.insert(INSERT, f'{server_username}: {server_message}\n', f'{server_username}')
+                self.window.MESSAGEBOX.configure(state='disabled')
             
-
+# <-------Window Class------->
 class Window:
-    def __init__(self,master:Tk, title,geometery):
+    def __init__(self,server:Server,master:Tk, title:str,geometery:str):
+        self.server=server
+        # Main window attr
         self.master = master
         master.title(title)
         master.geometry(geometery)
-
+        # Variables
         self.IP=StringVar(master)
         self.PORT = StringVar(master)
         self.USERNAME = StringVar(master)
         self.MESSAGE = StringVar(master)
-
+        # Frames
         self.room_frame = Frame(master)
         self.chat_frame = Frame(master)
-
-        self.login_btn= Button(master,text='login',command=self.login)
+        # Login Button
+        self.login_btn= Button(master,text='Login',command=self.login_window)
         self.login_btn.grid(column=2,row=2)
-
+        # Listbox room & dounble click event
         self.ROOMS_LIST = Listbox(self.room_frame)
         self.ROOMS_LIST.bind('<Double-1>',self.double_ckick_event)
         self.MESSAGEBOX = Text(self.chat_frame) 
 
-    def login(self):
-        # Login form
+    def login_window(self):
+        # login window attr
         win = Toplevel(self.master)
         win.title('Login')
         win.geometry('200x200')
-
-        # User entry & label
+        # login window items
         Label(win, text='Enter your IP').pack()
         Entry(win, textvariable=self.IP).pack()
         Label(win, text='Enter your Port').pack()
         Entry(win, textvariable=self.PORT).pack()
         Label(win, text='Enter your Username').pack()
         Entry(win, textvariable=self.USERNAME).pack()
-        Button(win, text='save', command=lambda: check_valid(win, self.IP.get(), self.PORT.get(), self.USERNAME.get())).pack()
+        Button(win, text='save', command=lambda: check_valid(win,self.server ,self.IP.get(), self.PORT.get(), self.USERNAME.get())).pack()
 
     def rooms_frame (self):
+        # TODO show username
         self.login_btn.destroy()
-
         self.room_frame.pack()
-
+        # Rooms' frame items
         Label(self.room_frame, text='Avabaile Rooms:').pack()
         self.ROOMS_LIST.pack()
-        Button(self.room_frame,text='Create Room',command=lambda:ser.client_create_room()).pack()
+        Button(self.room_frame,text='Create Room',command=lambda:self.server.client_create_room()).pack()
     
     def chats_frame (self):
+        # TODO show username and room's number
         self.chat_frame.pack()
-
+        # Chats' frame items
         Entry(self.chat_frame,textvariable=self.MESSAGE, width=50).pack()
-        Button(self.chat_frame,text='Send', command=lambda: ser.client_send(self.MESSAGE.get())).pack()
+        Button(self.chat_frame,text='Send', command=lambda: self.server.client_send_message(self.MESSAGE.get())).pack()
         self.MESSAGEBOX.pack()
     
     def insert_listbox(self,num:int):
@@ -160,30 +161,15 @@ class Window:
     
     def double_ckick_event(self,event):
         selected_room = self.ROOMS_LIST.curselection()
-        ser.RoomID = str(selected_room[0]+1)       
-
+        self.server.RoomID = str(selected_room[0]+1)       
         self.room_frame.destroy()
         self.chats_frame()
 
 
-# ------------------------------UI-------------------------------
+# ------------------------------Main-------------------------------
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ser = server(soc)
-
-
-
-
-
-
-
-
-
 tk = Tk()
-window = Window(tk,"323's Room","400x400")
+server = Server(soc)
+win = Window(server,tk,"323's Room","400x400")
 tk.mainloop()
-
-
-# ------------------------------Run--------------------------------
-
-
